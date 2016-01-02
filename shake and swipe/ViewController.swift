af2e421f8ad11840e93c562838f20c51a25f527f
yukisako <yukisakoatoz@gmail.com>
2016-01-02 15:33:01 +0900

    Initial Commit

---
+++ shake and swipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shake and swipe
+//
+//  Created by 迫 佑樹 on 2016/01/02.
+//  Copyright © 2016年 迫 佑樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
